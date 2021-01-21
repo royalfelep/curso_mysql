0310e0e2bc342ddebb94e884556ab116c41c9843
Felipe da Cruz <bsb.felipecorrea@gmail.com>
2021-01-21 17:13:01 -0300

    Adicionando restante dos exercícios concluídos

--- exercicios_select.sql
+++ exercicios_select.sql
@@ -3,15 +3,15 @@ show tables;
 select * from pessoas;
 
 /* Exercícios:
-1) Uma lista com o nome de todos os gafanhotos Mulheres.
+1) Uma lista com o nome de todas as mulheres
 2) Uma lista com os dados de todos aqueles que nasceram entre 1/Jan/2000 e 31/Dez/2015.
 3) Uma lista com o nome de todos os homens que trabalham como programadores.
 4) Uma lista com os dados de todas as mulheres que nasceram no Brasil e que têm seu nome iniciando com a letra J.
 5) Uma lista com o nome e nacionalidade de todos os homens que têm Silva no nome, não nasceram no Brasil e pesam menos de 100 Kg.
-6) Qual é a maior altura entre gafanhotos Homens que moram no Brasil?
-7) Qual é a média de peso dos gafanhotos cadastrados?
-8) Qual é o menor peso entre os gafanhotos Mulheres que nasceram fora do Brasil e entre 01/Jan/1990 e 31/Dez/2000?
-9) Quantas gafanhotos Mulheres tem mais de 1.90cm de altura?
+6) Qual é a maior altura entre os homens que moram no Brasil?
+7) Qual é a média de peso de todas as pessaos cadastradas?
+8) Qual é o menor peso entre as mulheres que nasceram fora do Brasil e entre 01/Jan/1990 e 31/Dez/2000?
+9) Quantas mulheres tem mais de 1.90cm de altura?
 */
 
 -- 1) retorna pessoas do sexo feminino;
